1e29a06bf479d4fc4b458bfeaf72d0db84ad864c
John Laceda <john.laceda@gmail.com>
2019-03-26 16:25:17 -0700

    separate out clues

---
+++ assets/javascript/clues.js
@@ -0,0 +1,168 @@
+// the clues
+// http://www.j-archive.com/showgame.php?game_id=5454
+let moneymoneymoney =
+{
+	name: "money, money, money",
+	clues:
+	[
+		{
+			value: 200,
+			answer: "The name of this British monetary unit comes from a unit of weight",
+			responses: [
+				"kilogram",
+				"farthing",
+				"pound",
+				"quid",
+			],
+			question: "pound",
+			used: false,
+			correct: false,
+		},
+		{
+			value: 400,
+			answer: "From 1792 to 1873 the U.S. issued silver 5-cent coins called not nickels but 'half' these",
+			responses: [
+				"quarter",
+				"dollar",
+				"nickel",
+				"dime",
+			],
+			question: "dime",
+			used: false,
+			correct: false,
+		},
+		{
+			value: 600,
+			answer: "The Lincoln Memorial is on the back of this U.S. bill",
+			responses: [
+				"$100",
+				"$50",
+				"$10",
+				"$5",
+			],
+			question: "$5",
+			used: false,
+			correct: false,
+		},
+		{
+			value: 800,
+			answer: "A silver tetradrachm from the 300s B.C. features a great portrait of him",
+			responses: [
+				"Apollo",
+				"Ptolemy",
+				"Alexander the Great",
+				"Pyrrhus of Epirus",
+			],
+			question: "Alexander the Great",
+			used: false,
+			correct: false,
+		},
+		{
+			value: 1000,
+			answer: "English settlers in the New World used this word from Algonquian for beads used as Indian money",
+			responses: [
+				"pence",
+				"wampum",
+				"roenoke",
+				"doubloon",
+			],
+			question: "wampum",
+			used: false,
+			correct: false,
+		}
+	]
+};
+
+let weregoingtothemovies = 
+{
+	name: "we're going to the movies",
+	clues:
+	[
+		{
+			value: 200,
+			answer: "My favorite part of going is watching the previews called these, like something a car pulls",
+			responses: [
+				"camper",
+				"literalize",
+				"trailers",
+				"screening",
+			],
+			question: "trailers",
+			used: false,
+			correct: false,
+		},
+		{
+			value: 400,
+			answer: "I love this format that gives \"up to 40% more image\" & \"highest quality 3D\"",
+			responses: [
+				"4K",
+				"IMAX",
+				"3D",
+				"BluRay",
+			],
+			question: "IMAX",
+			used: false,
+			correct: false,
+		},
+		{
+			value: 600,
+			answer: "The ticket guy knows me, so I'm front row center for this 2016 monster movie sequel with an address for a title",
+			responses: [
+				"10 Cloverfield Lane",
+				"Wolf on Wall Street",
+				"21 Jump Street",
+				"A Nightmare on Elm Street",
+			],
+			question: "10 Cloverfield Lane",
+			used: false,
+			correct: false,
+		},
+		{
+			value: 800,
+			answer: "I scream, you scream for these bite-sized chocolate-covered ice cream treats with a double talk name",
+			responses: [
+				"Mike and Ikes",
+				"Tutti Fruitti",
+				"BonBons",
+				"Jujyfruits",
+			],
+			question: "BonBons",
+			used: false,
+			correct: false,
+		},
+		{
+			value: 1000,
+			answer: "Before we go, we should check out the ratings on this website that sounds like something you throw at a bad film",
+			responses: [
+				"IMDB",
+				"Popcorn",
+				"Metacritic",
+				"Rotten Tomatoes",
+			],
+			question: "Rotten Tomatoes",
+			used: false,
+			correct: false,
+		}
+	]
+};
+
+let thechroniclesofnarnia = 
+{
+	name: "the chronicles of narnia",
+	clues:
+	[
+		{
+			value: 200,
+			answer: "",
+			responses: [
+				"",
+				"",
+				"",
+				"",
+			],
+			question: "",
+			used: false,
+			correct: false,
+		}
+	]
+};
